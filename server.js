455a369fea0a2f7fd3cf0a9c0da28d92451b36fa
scorpidio11 <scorpido@gmail.com>
2018-10-23 21:22:59 -0700

    code

--- server.js
+++ server.js
@@ -13,19 +13,90 @@ let express = require('express');
 let app = express();
 // any # 80 and above
 const PORT = 3000;
+let path = require("path");
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+let characters = [
+    
+    {routeName: "captainamerica", // routing no space
+    name: "Captain America",
+    role: "Saving the world",
+    age: 100,
+    strength: 1000
+},{
+    routeName: "babo",
+    name: "Babo",
+    role: "Study",
+    age: 50,
+    strength: 500
+},{
+    routeName: "mimi",
+    name: "Mimi",
+    role: "Cook",
+    age: 90,
+    strength: 2000
+}];
 // When someone visit main of my website do this '/' is root of webstie
 // defining route, CALL ROUTE
 // function takes two parameter request / response
 
 
-app.get('/', function(req, res){
-    res.send('Welcome to Avengers page!');
-  });
+// app.get('/', function(req, res){
+//     res.send('Welcome to Avengers page!');
+//   });
 
-app.listen(PORT, function(){
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+  });
+  
+  app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+  });
+  
+
+
+// SEE API REST standard /api/v1/characters/ 
+//:characterID (it became array)
+app.get("/api/v1/characters", function (req, res) {
+   //return res.json(characters);
+   return res.json(characters);
+});
+
+// request ID need to match with the 
+
+app.get("/api/v1/characters/:characterId", function (req, res){
+	
+	let characterId = req.params.characterId;
+	
+	console.log(characterId);
+	
+	for (let i = 0; i < characters.length; i++) {
+		if (characterId === characters[i].routeName){
+			return res.json(characters[i]);
+		}
+	}	
+	
+}); //do an HTTP GET
+
+
+app.post("/api/v1/characters", function (req, res) {
+    let newCharacter = req.body;
+    console.log(newCharacter);
+ });
+
+
+
+ app.listen(PORT, function(){
 
     console.log("Avengers on PORT " + PORT);
 });
+
 
 
---
+++ src.js
@@ -0,0 +1,45 @@
+
+   $("#search-btn").on("click", function() {
+     var searchedCharacter = $("#character-search").val().trim();
+
+     // Using a RegEx Pattern to remove spaces from searchedCharacter
+     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+     searchedCharacter = searchedCharacter.replace(/\s+/g, "").toLowerCase();
+
+     $.get("/api/v1/characters/" + searchedCharacter, function(data) {
+       console.log(data);
+       if (data) {
+         $("#stats").show();
+         $("#name").text(data.name);
+         $("#role").text(data.role);
+         $("#age").text(data.age);
+         $("#force-points").text(data.strength);
+       }
+       else {
+         $("#name").text("The force is not strong with this one. Your character was not found.");
+         $("#stats").hide();
+       }
+     });
+   });
+
+
+
+
+   // Question: What does this code do?
+   $("#add-btn").on("click", function(event) {
+     event.preventDefault();
+     var newCharacter = {
+       name: $("#name").val().trim(),
+       role: $("#role").val().trim(),
+       age: $("#age").val().trim(),
+       forcePoints: $("#force-points").val().trim()
+     };
+
+     // Question: What does this code do??
+     $.post("/api/characters", newCharacter)
+       .then(function(data) {
+         console.log("add.html", data);
+         alert("Adding character...");
+       });
+   });
+
